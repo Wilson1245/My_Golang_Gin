982599e4e6d27ce0becf9876e73bdf20761c63fd
Wilson <wilson052864@gmail.com>
2022-04-08 23:59:59 +0800

    Add EP5 program.

--- YoubuteGolangLearning/pojo/User.go
+++ YoubuteGolangLearning/pojo/User.go
@@ -11,14 +11,35 @@ type User struct {
 	Email    string `json:"UserEmail"`
 }
 
+// FindAllUsers
 func FindAllUsers() []User {
 	users := []User{}
 	db.DBconnect.Find(&users)
 	return users
 }
 
+// FindByUserId
 func FindByUserId(userId string) User {
 	user := User{}
 	db.DBconnect.Where("id = ?", userId).First(&user)
+	return user
+}
+
+// CreateUser
+func CreateUser(user User) User {
+	db.DBconnect.Create(&user)
+	return user
+}
+
+// DeleteUser
+func DeleteUser(userId string) User {
+	user := User{}
+	db.DBconnect.Where("id = ?", userId).Delete(&user)
+	return user
+}
+
+// UpdateUser
+func UpdateUser(userId string, user User) User {
+	db.DBconnect.Model(&user).Where("id = ?", userId).Updates(user)
 	return user
 }
--- YoubuteGolangLearning/service/UserService.go
+++ YoubuteGolangLearning/service/UserService.go
@@ -4,12 +4,11 @@ import (
 	"golangAPI/pojo"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-var userList = []pojo.User{}
+// var userList = []pojo.User{}
 
 // Get User
 func GetAllUser(c *gin.Context) {
@@ -39,41 +38,61 @@ func PostUser(c *gin.Context) {
 		c.JSON(http.StatusNotAcceptable, "Error : "+err.Error())
 		return
 	}
-	userList = append(userList, user)
-	c.JSON(http.StatusOK, "Successfully posted")
+	// userList = append(userList, user)
+	// c.JSON(http.StatusOK, "Successfully posted")
+	newUser := pojo.CreateUser(user)
+	c.JSON(http.StatusCreated, newUser)
 }
 
 // delete User
 
 func DeleteUser(c *gin.Context) {
-	userId, _ := strconv.Atoi(c.Param("id"))
-	for _, user := range userList {
-		log.Println(user)
+	// userId, _ := strconv.Atoi(c.Param("id"))
+	// for _, user := range userList {
+	// 	log.Println(user)
 
-		userList = append(userList[:userId], userList[userId+1:]...)
-		c.JSON(http.StatusOK, "Successfully deleted")
+	// 	userList = append(userList[:userId], userList[userId+1:]...)
+	// 	c.JSON(http.StatusOK, "Successfully deleted")
+	// 	return
+
+	// }
+	// c.JSON(http.StatusNotFound, "Error")
+	user := pojo.DeleteUser(c.Param("id"))
+	if user.Id == 0 {
+		c.JSON(http.StatusNotFound, "Error")
 		return
-
 	}
-	c.JSON(http.StatusNotFound, "Error")
+	c.JSON(http.StatusOK, "Successfully deleted")
 }
 
 // put User
 
 func PutUser(c *gin.Context) {
-	beforeUser := pojo.User{}
-	err := c.BindJSON(&beforeUser)
+	// beforeUser := pojo.User{}
+	// err := c.BindJSON(&beforeUser)
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, "Error")
+	// }
+	// userId, _ := strconv.Atoi(c.Param("id"))
+	// for key, user := range userList {
+	// 	if userId == user.Id {
+	// 		userList[key] = beforeUser
+	// 		log.Println(userList[key])
+	// 		c.JSON(http.StatusOK, "Successfully")
+	// 		return
+	// 	}
+	// }
+	// c.JSON(http.StatusNotFound, "Error")
+	user := pojo.User{}
+	err := c.BindJSON(&user)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, "Error")
+		return
 	}
-	userId, _ := strconv.Atoi(c.Param("id"))
-	for key, user := range userList {
-		if userId == user.Id {
-			userList[key] = beforeUser
-			log.Println(userList[key])
-			c.JSON(http.StatusOK, "Successfully")
-			return
-		}
+	user = pojo.UpdateUser(c.Param("id"), user)
+	if user.Id == 0 {
+		c.JSON(http.StatusNotFound, "Error")
+		return
 	}
-	c.JSON(http.StatusNotFound, "Error")
+	c.JSON(http.StatusOK, "Successfully")
 }
